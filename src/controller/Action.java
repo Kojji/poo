25453cabd3830ec844050ecf0efa7189900f6291
kojji <fernandokojiyama@gmail.com>
2019-12-03 13:11:57 -0400

    add: exception para movimentos fora da arena

--- src/controller/Action.java
+++ src/controller/Action.java
@@ -40,13 +40,27 @@ public class Action {
   public void runGame() {
     fileReading();
     Scanner userInputs = new Scanner(System.in);
-    System.out.printf("\nIndique a quantidade de jogadores para a partida: ");
-    int numPlayers = userInputs.nextInt();
+    int numPlayers = 0;
+    while(numPlayers == 0) {
+      try{
+        System.out.printf("\nIndique a quantidade de jogadores para a partida: ");
+        numPlayers = userInputs.nextInt();
+      }catch(java.util.InputMismatchException e) {
+        System.out.println("O valor digitado precisa ser numérico!");
+        userInputs.nextLine();
+      }
+    }
     int [] playersRobotIndex = new int [numPlayers];
     getRobotList().RobotListPrint();
     for(int i = 0; i < numPlayers; i++) {
-      System.out.printf("\nEntre com o indice do robo para o jogador " + (i+1) + ": ");
-      playersRobotIndex[i] = (userInputs.nextInt()-1); // exception por teclar não int
+      try{
+        System.out.printf("\nEntre com o indice do robo para o jogador " + (i+1) + ": ");
+        playersRobotIndex[i] = (userInputs.nextInt()-1);
+      }catch(java.util.InputMismatchException e) {
+        System.out.println("O valor digitado precisa ser numérico!");
+        userInputs.nextLine();
+        i--;
+      }
     }
     System.out.println(Arrays.toString(playersRobotIndex));
     createRobots(playersRobotIndex);
@@ -57,8 +71,10 @@ public class Action {
     
     int turno = 1;
     boolean stopCondition = false;
+
     while(true) { // execução do jogo
 
+      boolean pulaBloco = false;
       System.out.println("\nTurno Jogador " + turno);
       printSessionArena();
       if(players[turno - 1].getVirusDuration() > 0) {
@@ -74,7 +90,14 @@ public class Action {
       try {
         while(true) { 
           System.out.printf("\nJogador %d - indique quantas casas deseja andar (até %d): ", turno, maxMov);
-          userMov = userInputs.nextInt(); //InputMismatchException
+          try {
+            userMov = userInputs.nextInt(); //InputMismatchException
+          }catch(java.util.InputMismatchException e) {
+            System.out.printf("\nO valor precisa ser menor ou igual a %d e numérico", maxMov);
+            System.out.printf("\nSe um valor válido não for inserido em %d tentativas, dano será aplicado a seu robô!", 3 - counterExceedMovs);
+            counterExceedMovs++;
+            continue;
+          }
           if(userMov > maxMov) { 
             if(counterExceedMovs >= 3) {
               throw new KeyException("3 tentativas");
@@ -87,130 +110,148 @@ public class Action {
           else { break; }
         }
       } catch(KeyException e) {
-        //implica dano e encerra turno
-        e.printStackTrace();
+        System.out.println("Jogador " + turno + " não digitou um valor valido em " + e.getMessage() + ", dano será aplicado!");
+        pulaBloco = true;
+        System.out.println("Vida Restante: " + players[turno - 1].getLife());
+        players[turno - 1].decreaseLife(80);
       }
 
 
       counterExceedMovs = 0;
       int directionInInt = 0;
       userInputs.nextLine(); // consome \n do nextInt() utilizado anteriormente
-      try{
-        while(true) {
-          System.out.printf("\nJogador %d - Direção do movimento de %d casas(C - cima, B - baixo, E - esquerda, D - Direita): ", turno, maxMov);
-          String direction = userInputs.nextLine();
-          if(direction.equals("C") || direction.equals("B") || direction.equals("E") || direction.equals("D")) {
-            if(direction.equals("C")) {
-              directionInInt = 0;
-            } else if(direction.equals("B")) {
-              directionInInt = 1;
-            } else if(direction.equals("E")) {
-              directionInInt = 2;
-            } else if(direction.equals("D")){
-              directionInInt = 3;
+      if(!pulaBloco) {
+        try{
+          while(true) {
+            System.out.printf("\nJogador %d - Direção do movimento de %d casas(C - cima, B - baixo, E - esquerda, D - Direita): ", turno, maxMov);
+            String direction = userInputs.nextLine();
+            if(direction.equals("C") || direction.equals("B") || direction.equals("E") || direction.equals("D")) {
+              if(direction.equals("C")) {
+                directionInInt = 0;
+              } else if(direction.equals("B")) {
+                directionInInt = 1;
+              } else if(direction.equals("E")) {
+                directionInInt = 2;
+              } else if(direction.equals("D")){
+                directionInInt = 3;
+              }
+              break;
+            } else {
+              if(counterExceedMovs >= 3) {
+                throw new KeyException("3 tentativas");
+                //throw excession, da dano ao jogador e encerra sua vez
+              }
+              System.out.printf("\nDireção informada não é valida");
+              System.out.printf("\nSe um valor válido não for inserido em %d tentativas, dano será aplicado a seu robô!", 3 - counterExceedMovs);
+              counterExceedMovs++;
             }
-            break;
-          } else {
-            if(counterExceedMovs >= 3) {
-              throw new KeyException("3 tentativas");
-              //throw excession, da dano ao jogador e encerra sua vez
-            }
-            System.out.printf("\nDireção informada não é valida");
-            System.out.printf("\nSe um valor válido não for inserido em %d tentativas, dano será aplicado a seu robô!", 3 - counterExceedMovs);
-            counterExceedMovs++;
           }
+        } catch(KeyException e) {
+          System.out.println("Jogador " + turno + " não digitou um valor valido em " + e.getMessage() + ", dano será aplicado!");
+          players[turno - 1].decreaseLife(80);
+          System.out.println("Vida Restante: " + players[turno - 1].getLife());
+          pulaBloco = true;
         }
-      } catch(KeyException e) {
-        //implica dano e encerra turno
-        e.printStackTrace();
       }
       
-      for(int steps = 0; steps < userMov; steps++) {
-        int movementX = 0;
-        int movementY = 0;
-        int beforeX = players[turno-1].getPosition().getWidth();
-        int beforeY = players[turno-1].getPosition().getLength();
+      if(!pulaBloco) {
 
-        switch(directionInInt) {
-          case 0:
-            movementY = players[turno-1].getPosition().getLength();
-            movementX = players[turno-1].getPosition().getWidth() - 1;
-            break;
-          case 1:
-            movementY = players[turno-1].getPosition().getLength();
-            movementX = players[turno-1].getPosition().getWidth() + 1;
-            break;
-          case 2:
-            movementY = players[turno-1].getPosition().getLength() - 1;
-            movementX = players[turno-1].getPosition().getWidth();
-            break;
-          case 3:
-            movementY = players[turno-1].getPosition().getLength() + 1;
-            movementX = players[turno-1].getPosition().getWidth();
-            break;
-        }
-
-        // fazer a verificação se jogador vai pra fora da area
-        
-        if(sessionArena.getArenaIndex(movementX, movementY) != 0) {
-          if(sessionArena.getArenaIndex(movementX, movementY)/10 == 0) {
-            int oponent = sessionArena.getArenaIndex(movementX, movementY);
-            int [] newPositionOpenent = players[oponent - 1].moveOnEncounter(sessionArena);
-            players[oponent - 1].setPosition(new Arena(0, newPositionOpenent[0], newPositionOpenent[1]));
-            sessionArena.setArenaIndex(newPositionOpenent[0], newPositionOpenent[1], oponent);
-            System.out.println("Jogador "+ turno + " passou por cima do jogador " + oponent + ".");
-            System.out.println("Jogador " + oponent + "foi movido para outras coordenadas.");
+        for(int steps = 0; steps < userMov; steps++) {
+          int movementX = 0;
+          int movementY = 0;
+          int beforeX = players[turno-1].getPosition().getWidth();
+          int beforeY = players[turno-1].getPosition().getLength();
+          
+          boolean movimenta = true;
+          try{
+            switch(directionInInt) {
+              case 0:
+                movementY = players[turno-1].getPosition().getLength();
+                movementX = players[turno-1].getPosition().getWidth() - 1;
+                break;
+              case 1:
+                movementY = players[turno-1].getPosition().getLength();
+                movementX = players[turno-1].getPosition().getWidth() + 1;
+                break;
+              case 2:
+                movementY = players[turno-1].getPosition().getLength() - 1;
+                movementX = players[turno-1].getPosition().getWidth();
+                break;
+              case 3:
+                movementY = players[turno-1].getPosition().getLength() + 1;
+                movementX = players[turno-1].getPosition().getWidth();
+                break;
+            }
+            if(movementX > sessionArena.getWidth() || movementX < 0) {
+              throw new UnsupportedOperationException("movemento para fora da arena");
+            } else if(movementY >= sessionArena.getLength() || movementY < 0) {
+              throw new UnsupportedOperationException("movemento para fora da arena");
+            }
             
-          } else if(sessionArena.getArenaIndex(movementX, movementY)/10 == 1) {
-            SpecialItems weapon = getWeaponList().getWeaponList().get(sessionArena.getArenaIndex(movementX, movementY)%10);
-            System.out.println("Jogador " + turno + " encontrou a arma: " + weapon.getname() + " com dano - " + weapon.getdmgCoef() + " e alcance - " + weapon.getRange());
-            
-            boolean stopInputCond = true;
-            while(stopInputCond) {
-              System.out.println("deseja trocar pela arma atual? (" + players[turno - 1].getWeapon().getname() + ")? (S/N)");
-              String gunSwap = userInputs.nextLine();
-              if(gunSwap.equals("S")) {
-                players[turno - 1].setWpOnHold(getWeaponList().findWeaponIndex(players[turno - 1].getWeapon().getname()));
-                players[turno - 1].setWeapon(weapon);
-                stopInputCond = false;
-              } else if(gunSwap.equals("N")) {
-                players[turno - 1].setWpOnHold(sessionArena.getArenaIndex(movementX, movementY)%10);
-                stopInputCond = false;
+          }catch(UnsupportedOperationException e) {
+            System.out.println("Jogador " + turno + " tentou um " + e.getMessage() + ", dano será aplicado!");
+            players[turno - 1].decreaseLife(80);
+            System.out.println("Vida Restante: " + players[turno - 1].getLife());
+            movimenta = false;
+          }
+          
+          
+          if(movimenta) {
+            if(sessionArena.getArenaIndex(movementX, movementY) != 0) {
+              if(sessionArena.getArenaIndex(movementX, movementY)/10 == 0) {
+                int oponent = sessionArena.getArenaIndex(movementX, movementY);
+                int [] newPositionOpenent = players[oponent - 1].moveOnEncounter(sessionArena);
+                players[oponent - 1].setPosition(new Arena(0, newPositionOpenent[0], newPositionOpenent[1]));
+                sessionArena.setArenaIndex(newPositionOpenent[0], newPositionOpenent[1], oponent);
+                System.out.println("Jogador "+ turno + " passou por cima do jogador " + oponent + ".");
+                System.out.println("Jogador " + oponent + "foi movido para outras coordenadas.");
+                
+              } else if(sessionArena.getArenaIndex(movementX, movementY)/10 == 1) {
+                SpecialItems weapon = getWeaponList().getWeaponList().get(sessionArena.getArenaIndex(movementX, movementY)%10);
+                System.out.println("Jogador " + turno + " encontrou a arma: " + weapon.getname() + " com dano - " + weapon.getdmgCoef() + " e alcance - " + weapon.getRange());
+                
+                boolean stopInputCond = true;
+                while(stopInputCond) {
+                  System.out.println("deseja trocar pela arma atual? (" + players[turno - 1].getWeapon().getname() + ")? (S/N)");
+                  String gunSwap = userInputs.nextLine();
+                  if(gunSwap.equals("S")) {
+                    players[turno - 1].setWpOnHold(getWeaponList().findWeaponIndex(players[turno - 1].getWeapon().getname()));
+                    players[turno - 1].setWeapon(weapon);
+                    stopInputCond = false;
+                  } else if(gunSwap.equals("N")) {
+                    players[turno - 1].setWpOnHold(sessionArena.getArenaIndex(movementX, movementY)%10);
+                    stopInputCond = false;
+                  }
+                }
+              } else if(sessionArena.getArenaIndex(movementX, movementY)/10 == 2) {
+                SpecialItems bomb = getBombList().getBombList().get(sessionArena.getArenaIndex(movementX, movementY)%10);
+                System.out.println("Bomba Estourada!! " + bomb.getdmgCoef() + " de dano ao Jogador " + turno);
+                int result = players[turno-1].decreaseLife(bomb.getdmgCoef());
+                //if vida <= 0 stop condition = true e continue numPlayer -1 se = 1 jogador restante ganhou
+                System.out.println("Vida Restante: " + result);
+              } else {
+                SpecialItems virus = getVirusList().getVirusList().get(sessionArena.getArenaIndex(movementX, movementY)%10);
+                players[turno - 1].setVirusDmg(virus.getdmgCoef());
+                players[turno - 1].setVirusDuration(virus.getDuration());
+                System.out.println("Virus Contraido!! Por " + virus.getDuration() + " turnos, " + virus.getdmgCoef() + " de dano será aplicado!");
               }
             }
-          } else if(sessionArena.getArenaIndex(movementX, movementY)/10 == 2) {
-            SpecialItems bomb = getBombList().getBombList().get(sessionArena.getArenaIndex(movementX, movementY)%10);
-            System.out.println("Bomba Estourada!! " + bomb.getdmgCoef() + " de dano ao Jogador " + turno);
-            int result = players[turno-1].decreaseLife(bomb.getdmgCoef());
-            //if vida <= 0 stop condition = true e continue numPlayer -1 se = 1 jogador restante ganhou
-            System.out.println("Vida Restante: " + result);
-          } else {
-            SpecialItems virus = getVirusList().getVirusList().get(sessionArena.getArenaIndex(movementX, movementY)%10);
-            players[turno - 1].setVirusDmg(virus.getdmgCoef());
-            players[turno - 1].setVirusDuration(virus.getDuration());
-            System.out.println("Virus Contraido!! Por " + virus.getDuration() + " turnos, " + virus.getdmgCoef() + " de dano será aplicado!");
+
+            players[turno-1].setPosition(new Arena(0,movementX,movementY));
+            sessionArena.setArenaIndex(movementX, movementY, turno);
+            if(players[turno - 1].getWpOnHold() != -1 && players[turno - 1].getDropWp()) {
+              sessionArena.setArenaIndex(beforeX, beforeY, 10 + players[turno - 1].getWpOnHold());
+              players[turno - 1].setDropWp(false);
+              players[turno - 1].setWpOnHold(-1);
+    
+            } else if(players[turno - 1].getWpOnHold() != -1 && !players[turno - 1].getDropWp()){
+              sessionArena.setArenaIndex(beforeX, beforeY, 0);
+              players[turno - 1].setDropWp(true);
+            } else {
+              sessionArena.setArenaIndex(beforeX, beforeY, 0);
+            }
           }
         }
-
-        players[turno-1].setPosition(new Arena(0,movementX,movementY));
-        sessionArena.setArenaIndex(movementX, movementY, turno);
-        if(players[turno - 1].getWpOnHold() != -1 && players[turno - 1].getDropWp()) {
-          sessionArena.setArenaIndex(beforeX, beforeY, 10 + players[turno - 1].getWpOnHold());
-          players[turno - 1].setDropWp(false);
-          players[turno - 1].setWpOnHold(-1);
-
-        } else if(players[turno - 1].getWpOnHold() != -1 && !players[turno - 1].getDropWp()){
-          sessionArena.setArenaIndex(beforeX, beforeY, 0);
-          players[turno - 1].setDropWp(true);
-        } else {
-          sessionArena.setArenaIndex(beforeX, beforeY, 0);
-        }
-        //printSessionArena();
-        //compara posição em que vai mover de 1 a 1
-        // se achar algo onde ira mover
-          //aplica dano se houver
-          //pergunta para trocar arma se quiser
-          //mover outro robo que esteja na posição
       }
         
 
